7214556896ffc40e9e6d00997be283d6f062464d
Nathan Robinson <nrobinson2000@me.com>
2019-04-03 14:48:16 -0400

    Initial commit at 14:48 of 2019-04-03

---
+++ src/gemfarmer.ino
@@ -0,0 +1,49 @@
+/*
+ * Project gemfarmer
+ * Description:
+ * Author:
+ * Date:
+ */
+
+#define sizeFile 512
+
+// setup() runs once, when the device is first turned on.
+void setup()
+{
+  // Put initialization like pinMode and begin functions here.
+
+  Serial.begin(115200);
+  Serial1.begin(115200);
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop()
+{
+
+  // Variables
+  static char tempString[sizeFile];
+  int spot = 0;
+
+  // Clear the string if Serial1 is available
+  if (Serial1.available())
+  {
+    memset(tempString, 0, sizeof(tempString));
+  }
+
+  // Read Serial1 into the string
+  while (Serial1.available())
+  {
+    tempString[spot++] = Serial.read();
+    if (spot > sizeFile)
+      break;
+    delay(10); // Wait for Serial1
+  }
+
+  // Output
+  if (strlen(tempString) > 0)
+  {
+    Serial.printlnf("tempString Length: %d", strlen(tempString));
+    Serial.println("tempString Contents:");
+    Serial.println(tempString);
+  }
+}
